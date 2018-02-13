e8bb30ff844e899b8eb015e3c29d54a19a57ffcf
Matt Nuzzo <mdnuzz@yahoo.com>
2018-02-13 18:08:47 -0500

    Initial functionality

--- hw4.py
+++ hw4.py
@@ -1,3 +1,10 @@
 import requests
 import json
 
+def retrieve_github_info(userID):
+    base_url = 'https://api.github.com/users/' + str(userID) + '/repos' 
+    req = requests.get(base_url)
+    print req.json()
+
+if __name__ == '__main__':
+    retrieve_github_info('mnuzzo567')
