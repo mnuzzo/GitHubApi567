03f81b76fa39170ef3c97f3c4fb77b973ef27c84
Matt Nuzzo <mdnuzz@yahoo.com>
2018-02-13 18:48:00 -0500

    updated program and added test file

--- hw4.py
+++ hw4.py
@@ -2,9 +2,22 @@ import requests
 import json
 
 def retrieve_github_info(userID):
-    base_url = 'https://api.github.com/users/' + str(userID) + '/repos' 
-    req = requests.get(base_url)
-    print req.json()
+    base_repo_url = 'https://api.github.com/users/' + str(userID) + '/repos' 
+    repo_req = requests.get(base_repo_url, auth=('mnuzzo567','Nanoo96@SnoozeO14'))
+    
+    if repo_req.status_code != 200:
+        print 'Error: Could Not Reach Endpoint'
+    else:
+        print repo_req.json()
+        return repo_req
+
+
+def parse_dat_json(rawRepoJSON):
+    for repo in rawRepoJSON:
+        for project in repo:
+            print project["name"]
+
 
 if __name__ == '__main__':
-    retrieve_github_info('mnuzzo567')
+    temp = retrieve_github_info('mnuzzo567')
+    parse_dat_json(temp)
